7bacbe25f50ce4c7a8a23b85d0386577fbc60612
jeniasivets <evgeniasivets@gmail.com>
2025-06-18 00:43:19 +0200

    update

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,11 @@ function About(){
             <h4 className="section-title">About</h4>
             <p className="section-content">
                 I am a Machine Learning Researcher and Engineer specializing in
-                Computer Vision and Generative AI. With a solid mathematical background and extensive engineering experience in
-                developing advanced ML pipelines, including GANs, transformers, and Stable Diffusion,
-                I focus on enhancing model performance and efficiency.
+                Computer Vision and Generative AI. With a solid mathematical foundation and
+                broad engineering experience, I build and optimize advanced ML pipelines using GANs,
+                transformers, and diffusion models. I focus on improving model performance, and efficiency in
+                practical applications. I also value the clear communication of complex ideas across technical and
+                non-technical domains.
             </p>
         </div>
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,11 @@
 import Profile from './Profile'
 import About from './About'
-import Interests from './Interests'
 import SocialMedia from './SocialMedia'
 function Card(){
     return(
         <div className="card">
         <Profile />
         <About />
-        <Interests />
         <SocialMedia />
         </div>
     );
